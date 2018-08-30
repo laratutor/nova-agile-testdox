7116ec4cb9150d4d22a60b06b9fee3334cf52fff
James LaChance <fatboyxpc@gmail.com>
2018-08-30 00:31:50 -0400

    Add dd() test untility

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,13 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
+    public function dd(...$variables)
+    {
+        foreach ($variables as $variable) {
+            print_r($variable);
+            echo PHP_EOL;
+        }
 
+        die();
+    }
 }
